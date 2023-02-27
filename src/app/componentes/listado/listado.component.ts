aa79e5a52f82951f8a18fda736d04c5cf5953c51
Javier Lete <jglete@eskura21.com>
2023-02-27 18:53:49 +0100

    Modelo de vista listado con petición al servicio

--- src/app/componentes/listado/listado.component.ts
+++ src/app/componentes/listado/listado.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Video } from 'src/app/modelos/video';
+import { VideoService } from 'src/app/servicios/video.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit {
+  videos: Video[] = [];
+  
+  constructor(private videoService: VideoService) {}
 
+  ngOnInit(): void {
+      this.videoService.obtenerTodos().subscribe(videos => this.videos = videos);
+  }
 }
