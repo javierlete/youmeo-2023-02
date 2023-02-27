4aef637de5e85dcd8c1697386e2db9073bfea781
Javier Lete <jglete@eskura21.com>
2023-02-27 20:20:58 +0100

    Detalle

--- src/app/componentes/detalle/detalle.component.ts
+++ src/app/componentes/detalle/detalle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Video } from 'src/app/modelos/video';
 import { VideoService } from 'src/app/servicios/video.service';
@@ -10,11 +11,15 @@ import { VideoService } from 'src/app/servicios/video.service';
 })
 export class DetalleComponent implements OnInit {
   video?: Video;
+  urlSaneada?: SafeResourceUrl;
   
-  constructor(private videoService: VideoService, private route: ActivatedRoute) {}
+  constructor(private videoService: VideoService, private route: ActivatedRoute, private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
       const id: number = Number(this.route.snapshot.paramMap.get('id'));
-      this.videoService.obtenerPorId(id).subscribe(video => this.video = video)
+      this.videoService.obtenerPorId(id).subscribe(video => {
+        this.video = video;
+        this.urlSaneada = this.sanitizer.bypassSecurityTrustResourceUrl(this.video.url);
+      });
   }
 }
