4bc7e8f43024d632cda6f511f9a85c6501516af1
Javier Lete <jglete@eskura21.com>
2023-02-27 19:07:01 +0100

    Modelo de vista detalle

--- src/app/componentes/detalle/detalle.component.ts
+++ src/app/componentes/detalle/detalle.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Video } from 'src/app/modelos/video';
+import { VideoService } from 'src/app/servicios/video.service';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styleUrls: ['./detalle.component.css']
 })
-export class DetalleComponent {
+export class DetalleComponent implements OnInit {
+  video?: Video;
+  
+  constructor(private videoService: VideoService, private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+      const id: number = Number(this.route.snapshot.paramMap.get('id'));
+      this.videoService.obtenerPorId(id).subscribe(video => this.video = video)
+  }
 }
